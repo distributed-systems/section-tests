2dbce9cea2a18ad6939b4435b0d9c75b71b4d748
Lina <lina@vanderweg.ch>
2021-08-30 21:40:32 +0200

    fix: main file points to npm source

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import TestRunner from '../src/TestRunner.js';
+import TestRunner from '../npm/src/TestRunner.js';
 import path from 'path';
 
 
@@ -16,7 +16,6 @@ process.on('unhandledRejection', (err, p) => {
 
 
 process.on('uncaughtException', console.trace);
-
 
 
 
